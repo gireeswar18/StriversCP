9259c9656469a9f6f673b716dfbd57595bb2245d
gireeswar18 <gireeswarcp18@gmail.com>
2026-07-08 08:03:02 +0530

    08/07/2026

---
+++ Buttons.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ * Buttons
+ */
+public class Buttons {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int wrong = 0;
+
+        for (int i = 1; i < n; i++) {
+            wrong += (n - i) * i;
+        }
+
+        System.out.println(wrong + n);
+
+        sc.close();
+    }
+}
---
+++ DonutShops.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class DonutShops {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- != 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            if (a < c) {
+                System.out.print(1 + " ");
+            }
+            else {
+                System.out.print(-1 + " ");
+            }
+
+            if (a * b > c) {
+                System.out.println(b);
+            }
+            else {
+                System.out.println(-1);
+            }
+
+        }
+
+        sc.close();
+    }
+}
